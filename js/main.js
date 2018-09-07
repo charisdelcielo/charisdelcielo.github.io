4c378f47ba1e903e25da33ab9467edabb1fd06b4
charisdelcielo <chaewonryu@gmail.com>
2018-09-07 14:54:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/* sticky navigation*/
+$(document).ready(function () {
+  var $nav = $('#navigation'),
+      posTop = $nav.position().top;
+  $(window).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > posTop) { $nav.addClass('fixed'); }
+    else { $nav.removeClass('fixed'); }
+  });
+});
+
+
+
 /* shrink header*/
 $(document).on("scroll", function() {
     if ($(document).scrollTop() > 100) {
