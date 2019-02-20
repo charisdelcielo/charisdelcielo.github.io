80edf9241a2899a9a88038bb534c01da85ae00d3
charisdelcielo <chaewonryu@gmail.com>
2019-02-20 03:38:54 -0600

    today revision

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,10 +1,57 @@
 /* side bar nav */
 var nav = document.getElementById("navigation");
+var menu = document.getElementById("menu-icon");
+var menuList = document.getElementById("horizontal-list");
 
+var offsetMenu = menu.offsetTop;
+
+function openNavBar() {
+  if (menu.classList.contains("open")) {
+    menuList.style.opacity = "0.5"
+    nav.style.width = "0";
+    menu.classList.remove("open");
+  }
+
+  else {
+    nav.style.width = "270px";
+    menu.classList.add("open");
+    setTimeout(function(){
+        menuList.style.opacity = "1";
+      },350);
+  }
+
+};
+
+
+
+
+/*sticky menu icon*/
+window.onscroll = function() {
+  stickyFunction();
+};
+
+function stickyFunction() {
+  if (window.pageYOffset > offsetMenu) {
+    menu.classList.add("fixed")
+  }
+  else {
+    menu.classList.remove("fixed")
+  }
+}
+
+/* download opico */
+document.getElementById("opico-apple").onclick = function() {
+  window.open("https://itunes.apple.com/us/app/opico/id1215993252");
+}
+document.getElementById("opico-android").onclick = function() {
+  window.open("https://play.google.com/store/apps/details?id=com.opico&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1");
+}
+
+/*
 document.getElementsByClassName("menu-icon").onclick = function() {
-  nav.classList.add("opened")
+  nav.classList.add("opened");
 };
 
 document.getElementById("close-icon").onclick = function() {
-  nav.classList.remove("opened")
-};
+  nav.classList.remove("opened");
+};*/
